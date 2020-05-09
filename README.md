f84943a97e5964af0076166304cf42f607216c1d
RaghuJain <raghujain108@gmail.com>
2020-05-09 22:37:03 +0530

    Added README file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# elocals
+# eLocals Project
+
+This project has features such as
+- Login
+- Sign Up
+- Dummy page which opens only if  user is logged in.
+- Logout functionality
+- Session timeout of 30 seconds
+- H2 Database support
+- Loggging Functionality
+- Front end developed using Bootstrap Framework.
+- Thymeleaf layouts
+
+Clone the code locally.
+Update the maven dependencies using pom.xml
+
+Run the project in Spring Tool Suite.
+
+- Open the website home page at : http://localhost:9090
+- Register your self at: http://localhost:9090/signup
+- Login: http://localhost:9090/login
+- Dummy Page: http://localhost:9090/welcome
+
+
--- eLocals/src/main/java/com/eLocals/Controller/UserSecurityController.java
+++ eLocals/src/main/java/com/eLocals/Controller/UserSecurityController.java
@@ -1,11 +1,8 @@
 package com.eLocals.Controller;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.eLocals.Entity.UserAccount;
 import com.eLocals.Repository.AccountRepository;
-
-
-
 @Controller
-
 public class UserSecurityController {
 	
 	private static final  Logger LOGGER=LoggerFactory.getLogger(UserSecurityController.class);
